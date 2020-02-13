c2c1a42d342882443714972b19f18ab9ad6ad6fc
atiwatice <48309112+atiwatice@users.noreply.github.com>
2020-02-13 08:51:37 +0700

    operation
    add new operation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.function.DoubleToIntFunction;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -29,6 +33,41 @@ public class Main {
         System.out.println(result);
         result-=2;
         System.out.println(result);
+        boolean isAlien = false;
+        if(isAlien==true) {
+            System.out.println("is not Alien");
+            System.out.println("-----");
+        }else{
+            System.out.println("ice");
+        }
+        int topScore = 80;
+        if(topScore<=100){
+            System.out.println("You got a high score");
+        }
+        int secondTopscore = 60;
+        if((topScore>secondTopscore) && (topScore<100)){
+            System.out.println("Greater than second top score");
+        }
+
+        if((topScore>90)||(secondTopscore<=90)){
+            System.out.println("either or both are true");
+        }
+        int newValue = 50;
+        if(newValue==50){
+            System.out.println("This is true");
+        }
+        boolean isCar =false;
+        if(!isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true:false;
+        
+        if(wasCar){
+            System.out.println("This is car");
+        }
+
 
     }
 }
